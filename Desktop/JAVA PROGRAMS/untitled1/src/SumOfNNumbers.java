69e005d8a7f957cc5d106406d7d12b5df2959327
Manjunath-5717 <manjunathragati704@gmail.com>
2026-02-03 16:58:41 +0530

    add sumof N numbers Java program

---
+++ Desktop/JAVA PROGRAMS/untitled1/src/SumOfNNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfNNumbers {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter N numbres");
+        int n = sc.nextInt();
+
+        int sum=0;
+
+        for(int i=0;i<=n;i++){
+            sum =sum+i;
+        }
+        System.out.println("Sum of first "+ n +"sum is :"+ sum);
+
+        sc.close();
+    }
+}
